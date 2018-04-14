b1173bd28ba5372639d879e0284d2e9c800bbc2f
allen05ren <allen05ren@outlook.com>
2018-04-14 13:16:41 +0800

    替换不小心又引入的制表符

--- wemark/wemark.js
+++ wemark/wemark.js
@@ -55,31 +55,31 @@ function parse(md, page, options) {
             }
         } else {
             inlineToken.children.forEach(function(token, index) {
-				switch (token.type) {
-					case 'text':
-					case 'code':
-						ret.push({
-							type: env || token.type,
-							content: token.content
-						});
-						env = '';
-						break;
-					case 'del_open':
-						env = 'deleted';
-						break;
-					case 'strong_open':
-						env = 'strong';
-						break;
-					case 'em_open':
-						env = 'em';
-						break;
-					case 'image':
-						ret.push({
-							type: token.type,
-							src: token.src
-						});
-						break;
-				}
+                switch (token.type) {
+                    case 'text':
+                    case 'code':
+                        ret.push({
+                            type: env || token.type,
+                            content: token.content
+                        });
+                        env = '';
+                        break;
+                    case 'del_open':
+                        env = 'deleted';
+                        break;
+                    case 'strong_open':
+                        env = 'strong';
+                        break;
+                    case 'em_open':
+                        env = 'em';
+                        break;
+                    case 'image':
+                        ret.push({
+                            type: token.type,
+                            src: token.src
+                        });
+                        break;
+                }
             });
         }
 
@@ -87,101 +87,101 @@ function parse(md, page, options) {
     };
 
     var getBlockContent = function(blockToken, index) {
-		switch (blockToken.type) {
-			case 'htmlblock':
-				return getInlineContent(blockToken);
-			case 'heading_open':
-				return {
-					type: 'h' + blockToken.hLevel,
-					content: getInlineContent(tokens[index+1])
-				};
-			case 'paragraph_open':
-				var type = 'p';
-				var prefix = '';
-				if (env.length) {
-					prefix = env.join('_') + '_';
-				}
+        switch (blockToken.type) {
+            case 'htmlblock':
+                return getInlineContent(blockToken);
+            case 'heading_open':
+                return {
+                    type: 'h' + blockToken.hLevel,
+                    content: getInlineContent(tokens[index+1])
+                };
+            case 'paragraph_open':
+                var type = 'p';
+                var prefix = '';
+                if (env.length) {
+                    prefix = env.join('_') + '_';
+                }
 
-				var content = getInlineContent(tokens[index+1]);
+                var content = getInlineContent(tokens[index+1]);
 
-				// 处理ol前的数字
-				if (env[env.length - 1] === 'li' && env[env.length - 2] === 'ol') {
-					content.unshift({
-						type:'text',
-						content:orderNum[listLevel - 1] + '. '
-					});
-				}
+                // 处理ol前的数字
+                if (env[env.length - 1] === 'li' && env[env.length - 2] === 'ol') {
+                    content.unshift({
+                        type:'text',
+                        content:orderNum[listLevel - 1] + '. '
+                    });
+                }
 
-				return {
-					type: prefix + 'p',
-					content: content
-				};
-			case 'fence':
-				return {
-					type: 'code',
-					content: blockToken.content
-				};
-			case 'bullet_list_open':
-				env.push('ul');
-				listLevel++;
-				break;
-			case 'ordered_list_open':
-				env.push('ol');
-				listLevel++;
-				break;
-			case 'list_item_open':
-				env.push('li');
-				if (env[env.length - 2] === 'ol' ) {
-					orderNum[listLevel - 1]++;
-				}
-				break;
-			case 'list_item_close':
-				env.pop();
-				break;
-			case 'bullet_list_close':
-				env.pop();
-				listLevel--;
-				break;
-			case 'ordered_list_close':
-				env.pop();
-				listLevel--;
-				orderNum[listLevel] = 0;
-				break;
-			case 'blockquote_open':
-				env.push('blockquote');
-				break;
-			case 'blockquote_close':
-				env.pop();
-				break;
-			case 'tr_open':
-				tmp = {
-					type: 'table_tr',
-					content:[]
-				};
-				return tmp;
-			case 'th_open':
-				tmp.content.push({
-					type: 'table_th',
-					content: getInlineContent(tokens[index+1]).map(function(inline) {return inline.content;}).join('')
-				});
-				break;
-			case 'td_open':
-				tmp.content.push({
-					type: 'table_td',
-					content: getInlineContent(tokens[index+1]).map(function(inline) {return inline.content;}).join('')
-				});
-				break;
-		}
+                return {
+                    type: prefix + 'p',
+                    content: content
+                };
+            case 'fence':
+                return {
+                    type: 'code',
+                    content: blockToken.content
+                };
+            case 'bullet_list_open':
+                env.push('ul');
+                listLevel++;
+                break;
+            case 'ordered_list_open':
+                env.push('ol');
+                listLevel++;
+                break;
+            case 'list_item_open':
+                env.push('li');
+                if (env[env.length - 2] === 'ol' ) {
+                    orderNum[listLevel - 1]++;
+                }
+                break;
+            case 'list_item_close':
+                env.pop();
+                break;
+            case 'bullet_list_close':
+                env.pop();
+                listLevel--;
+                break;
+            case 'ordered_list_close':
+                env.pop();
+                listLevel--;
+                orderNum[listLevel] = 0;
+                break;
+            case 'blockquote_open':
+                env.push('blockquote');
+                break;
+            case 'blockquote_close':
+                env.pop();
+                break;
+            case 'tr_open':
+                tmp = {
+                    type: 'table_tr',
+                    content:[]
+                };
+                return tmp;
+            case 'th_open':
+                tmp.content.push({
+                    type: 'table_th',
+                    content: getInlineContent(tokens[index+1]).map(function(inline) {return inline.content;}).join('')
+                });
+                break;
+            case 'td_open':
+                tmp.content.push({
+                    type: 'table_td',
+                    content: getInlineContent(tokens[index+1]).map(function(inline) {return inline.content;}).join('')
+                });
+                break;
+        }
     };
 
     tokens.forEach(function(token, index) {
-		var blockContent = getBlockContent(token, index);
+        var blockContent = getBlockContent(token, index);
 
-		if (!blockContent) return;
+        if (!blockContent) return;
 
         if (!Array.isArray(blockContent)) {
             blockContent = [blockContent];
-		}
+        }
 
         blockContent.forEach(function(block) {
             if (Array.isArray(block.content)) {
