29dcc886cbdfafa83f7e9ced8a6fb226bfe0ef94
TooBug <i@toobug.net>
2018-07-06 10:17:55 +0800

    支持小程序内链接跳转 close #37

--- demo/wemark/wemark.js
+++ demo/wemark/wemark.js
@@ -8,16 +8,25 @@ Component({
             observer(){
                 this.parseMd();
             }
-        }
+        },
+		link: {
+			type: Boolean,
+			value: false
+		}
     },
     data: {
         parsedData: {}
     },
     methods: {
         parseMd(){
+			console.log(this.data.link, parser.parse(this.data.md, {
+				link: this.data.link
+			}));
             if(this.data.md){
                 this.setData({
-                    parsedData: parser.parse(this.data.md)
+                    parsedData: parser.parse(this.data.md, {
+						link: this.data.link
+					})
                 });
             }
         }
